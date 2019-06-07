14642ec77299839aa0e5ac5f125254badb7878b3
Shankara <sanand@broadinstitute.org>
2019-06-07 23:19:26 +0000

    added samplers

---
+++ prep.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Eukaryota/UP000005640_9606.fasta.gz
+wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.2bit
