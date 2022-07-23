85dcf410eb40d53d3716c85480edf40d474d0b33
Julian Hess <jhess@broadinstitute.org>
2022-07-23 12:26:41 -0400

    Workflow natively passes directories between tasks

--- wolF/tasks.py
+++ wolF/tasks.py
@@ -52,9 +52,6 @@ class clumps_run_task(wolf.Task):
 
     overrides = { "prot2pdb_chunks" : "delayed" }
 
-    ### bash script to run the `run-step` ###
-    # un-tar the `clumps_preprocess.tar` file which is the output directory from the 'clumps-prep step'
-    # this will create a diretory(folder) of the same name (clumps_preprocess)
     script = """    
     clumps --muts ${clumps_preprocess}/split_proteins \
         --maps ${prot2pdb_chunks} \
@@ -72,36 +69,28 @@ class clumps_run_task(wolf.Task):
     resources = { "cpus-per-task" : 8 }
 
     output_patterns = {
-        "run_outdir" : "clumps_results.tar"
+        "run_outdir" : "clumps_results"
     }
     
     docker = CLUMPS_DOCKER_IMAGE
 
 class clumps_postprocess_task(wolf.Task):
     # Generates summary files from array outputs of clumps.
-    # gather task which takes an array of inputs, but rather than dispatch an individual job for each input,
-    # instead gather them together.
     resources = { "mem" : "8G" }
     
-    # <Required> Results directory from CLUMPS
-    # <Required> Split protein directory with mutation mapping information.
     inputs = {
         "clumps_preprocess" : None,
-        "clumps_results" : None, #gather_param (paths to all input files being gathered)
+        "clumps_results" : None, # this is an array of directories, which in turn contain multiple files
         "cancer_genes" : None,
         "uniprot_map" : None,
         "pdb_dir" : None
     }
     
-    ### bash script to run the `post-step` ###
-    # untar the 'clumps_preprocess' to access the files
-    # untar the 'clumps_results' to access the input files
     script = """
-    #tar xf $clumps_preprocess
-    #while read -r i; do
-    #  tar xf $i
-    #done < $clumps_results
-    clumps-postprocess --input_dir ${clumps_results} \
+    # gather contents of all clumps results directories into single directory
+    mkdir results_gather
+    cat ${clumps_results} | xargs -I{} find -L {} -type f -not -path "*/.*" | xargs -P100 -I{} ln -s {} results_gather
+    clumps-postprocess --input_dir results_gather \
       --proteins_dir ${clumps_preprocess}/split_proteins \
       --cancer_genes ${cancer_genes} \
       --uniprot_map ${uniprot_map} \
@@ -120,23 +109,24 @@ class clumps_postprocess_task(wolf.Task):
 ###### workflow
 def clumps_workflow(
   maf,
+  sampler,
   genome_2bit = "gs://sa-clumps2-ref/dat/hg19.2bit",
   fasta = "gs://sa-clumps2-ref/dat/UP000005640_9606.fasta.gz",
   gpmaps = "gs://sa-clumps2-ref/dat/genomeProteomeMaps.txt",
   prot2pdb_chunks = "gs://sa-clumps2-ref/dat/huniprot/huniprot2pdb.run18_chunks/",
   pdb_dir = "gs://sa-clumps2-ref/dat/pdbs/ftp.wwpdb.org/pub/pdb/data/structures/divided/pdb",
-  coverage_track = "gs://sa-clumps2-ref/dat/cov/WEx_cov.fwb",
+  coverage_track = "gs://sa-clumps2-ref/dat/cov/WEx_cov.fwb", # TODO: need to specify index here too, and add it to the localization task
   cancer_genes = "gs://sa-clumps2-ref/dat/allCancerGenes.txt",
   uniprot_map = "gs://sa-clumps2-ref/dat/huniprot/huniprot2pdb.run18.filt.txt"
 ):
-    # (step-#0): localization task
+    # localization task
     localization = wolf.LocalizeToDisk(
       files = {
         "maf" : maf,
         "genome_2bit" : genome_2bit,
         "fasta" : fasta,
         "gpmaps" : gpmaps,
-        "prot2pdb_chunks" : prot2pdb_chunks,
+        "prot2pdb_chunks" : prot2pdb_chunks, # XXX: do we need this on the RODISK? we are scattering directly from the bucket
         "pdb_dir" : pdb_dir,
         "coverage_track" : coverage_track,
         "cancer_genes" : cancer_genes,
@@ -144,6 +134,7 @@ def clumps_workflow(
       }
     )
 
+    # map genome to proteome
     clumps_prep = clumps_prep_task(
       inputs = {
         "maf" : localization["maf"],
@@ -153,15 +144,36 @@ def clumps_workflow(
       }
     )
 
-#    # (step-#2): run task
-#    clumps_run = clumps_run_task(
-#      inputs = {
-#        "clumps_preprocess" : clumps_prep["prep_outdir"],
-#        "prot2pdb_chunks" : localization["prot2pdb_chunks"] chunks_list_18,
-#        "pdb_dir" : localization["pdb_dir"],
-#        "coverage_track" : localization["coverage_track"],
-#        "genome_2bit" : localization["genome_2bit"],
-#        "fasta" : localization["fasta"],
-#        "gpmaps" : localization["gpmaps"]
-#      }
-#    )
+    # list chunks to define scatter
+    # (since we can't natively scatter against a directory)
+    chunk_list = wolf.Task(
+      name = "list_chunks",
+      inputs = { "chunks" : prot2pdb_chunks },
+      overrides = { "chunks" : "string" },
+      script = "gsutil ls ${chunks} > chunks.txt",
+      outputs = { "chunks" : ("chunks.txt", wolf.read_lines) }
+    )
+
+    # run clumps on each shard
+    clumps_run = clumps_run_task(
+      inputs = {
+        "clumps_preprocess" : clumps_prep["prep_outdir"],
+        "prot2pdb_chunks" : chunk_list["chunks"],
+        "pdb_dir" : localization["pdb_dir"],
+        "coverage_track" : localization["coverage_track"],
+        "coverage_track_index" : coverage_track[:-4] + ".fwi", # FIXME: hack to avoid recreating the rodisk
+        "genome_2bit" : localization["genome_2bit"],
+        "fasta" : localization["fasta"],
+        "gpmaps" : localization["gpmaps"]
+      }
+    )
+
+    clumps_postprocess = clumps_postprocess_task(
+      inputs = {
+        "clumps_preprocess" : clumps_prep["prep_outdir"],
+        "clumps_results" : [clumps_run["run_outdir"]],
+        "cancer_genes" : localization["cancer_genes"],
+        "uniprot_map" : localization["uniprot_map"],
+        "pdb_dir" : localization["pdb_dir"]
+      }
+    )
